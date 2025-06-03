3144a3f2bcb37ed943cf7acc9e22794dfe732370
a-ru0110 <rei060110@gmail.com>
2025-06-03 15:00:35 +0900

    タイトルのサイズ戻し

--- Assets/Script/CSVs/CSVReader.cs
+++ Assets/Script/CSVs/CSVReader.cs
@@ -60,6 +60,8 @@ public class CSVReader : MonoBehaviour
 
     void Start()
     {
+        playerStatus_Script = GetComponent<PlayerStatus_Script>();
+        enemyStatus_Script = GetComponent<EnemyStatus_Script>();
         CsvRead(enemyStatusCSV, enemyStatusDatas);
     }
 

b7cc6c73a803f25e66785139ac7f654bc14db2d9
a-ru0110 <rei060110@gmail.com>
2025-06-16 14:42:02 +0900

    エフェクトとマップ更新

---
+++ Assets/Script/Effects/PlayerEffectScript1.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerEffectScript : MonoBehaviour
+{
+    [SerializeField, Header("エフェクトオブジェクト")] GameObject[] playerEffect;
+
+    public enum EffectName
+    {
+        AVOIDANCE = 0,
+        SMOKE,
+        SLASH,
+        AURA
+    }
+
+    void Start()
+    {
+        Init();
+    }
+
+    void Update()
+    {
+
+    }
+
+    //全部非稼働にする
+    private void Init()
+    {
+        for (int i = 0; i < playerEffect.Length; i++)
+        {
+            playerEffect[i].SetActive(false);
+        }
+    }
+
+    public void PlayEffect(int effectNo)
+    {
+        playerEffect[effectNo].SetActive(true);
+
+    }
+
+    public void StopSmokeEffect()
+    {
+        playerEffect[(int)EffectName.SMOKE].SetActive(false);
+    }
+
+    public void StopAvoidanceEffect()
+    {
+        playerEffect[(int)EffectName.AVOIDANCE].SetActive(false);
+    }
+}

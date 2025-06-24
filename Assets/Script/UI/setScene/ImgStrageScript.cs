dc7e9d7baa6059142b9fefa2b191d4517ace886e
koumei_humantest-admin <matayosikoumei@gmail.com>
2025-06-24 14:11:16 +0900

    komeふぁいるからうつしたよ

---
+++ Assets/Script/UI/setScene/ImgStrageScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImgStrageScript : MonoBehaviour
+{
+
+    public Sprite[] sprites;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

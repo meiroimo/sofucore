05a89ae8cbc5d6e10f428885dcc1b52478df5236
meiroimo <mei04510@outlook.jp>
2025-11-25 15:28:13 +0900

    スポーン間隔＆出現数調整

---
+++ Assets/NewScript/Difficulty.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Difficulty
+{
+    static float secondsToMaxDifficulty = 120;
+
+    public static float GetDifficultyPercent()
+    {
+        return 1;
+        return Mathf.Clamp01(Time.time / secondsToMaxDifficulty);
+    }
+}

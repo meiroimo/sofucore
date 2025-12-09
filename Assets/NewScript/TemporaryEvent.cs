e58ca61158cc42fec3d5622682285eea3f7f5977
meiroimo <mei04510@outlook.jp>
2025-12-09 14:44:49 +0900

    イベント仮追加

---
+++ Assets/NewScript/TemporaryEvent.cs
@@ -0,0 +1,51 @@
+using System;
+using UnityEngine;
+
+public class TemporaryEvent
+{
+    private float duration;//効果が続く時間
+    private Action applyEffect;//効果が発動した時の処理
+    private Action removeEffect;//効果が解除された時の処理
+    private float timer;//時間計測用
+    private bool active;//今動いてるか
+
+    public event Action OnEventFinished;
+
+    public TemporaryEvent(float duration, Action apply, Action remove)
+    {
+        this.duration = duration;//効果が続く時間
+        this.applyEffect = apply;//効果が発動した時の処理
+        this.removeEffect = remove;//効果が解除された時の処理
+    }
+
+    public void Activate()
+    {
+        Debug.LogWarning("イベント開始");
+        if(applyEffect != null)
+        {
+            applyEffect.Invoke();
+        }
+        timer = 0f;
+        active = true;
+    }
+
+    public void Update(float deltaTime)
+    {
+        if (!active) return;
+
+        timer += deltaTime;
+
+        if(timer >= duration)
+        {
+            if(removeEffect != null)
+            {
+                removeEffect.Invoke();
+                active = false;
+                Debug.LogWarning("イベント終了");
+                OnEventFinished?.Invoke(); // ←イベント終了を通知
+            }
+        }
+    }
+
+    public bool IsActive => active;//外から読める
+}

ac4a66e639c10a522e1cc65836defa3cf423facc
a-ru0110 <rei060110@gmail.com>
2025-12-16 14:38:52 +0900

    ひっかきに合わせたエフェクト追加

---
+++ Assets/Script/Effects/SpecialMoveEffectScript.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpecialMoveEffectScript : MonoBehaviour
+{
+    [SerializeField] GameObject leftEffect;
+    [SerializeField] GameObject rightEffect;
+
+
+    void Start()
+    {
+        leftEffect.SetActive(false);
+        rightEffect.SetActive(false);
+    }
+
+    public void StartLeftEffect()
+    {
+        leftEffect.SetActive(false);
+        leftEffect.SetActive(true);
+    }
+
+    public void StartRightEffect()
+    {
+        rightEffect.SetActive(false);
+        rightEffect.SetActive(true);
+    }
+
+}
